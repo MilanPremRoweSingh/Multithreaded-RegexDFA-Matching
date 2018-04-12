8b22327af8067d5705a7d1bdd27a3b62bbb1fa4f
Milan Singh <milanprsingh@gmail.com>
2018-04-12 10:05:33 -0400

    Created faster DFA which uses indexing instead of an unordered map, and direct pointer usage opposed to vectors. Now there is speedup for n>0 optimistic threads

--- Multithreaded-RegexDFA-Matching.cpp
+++ Multithreaded-RegexDFA-Matching.cpp
@@ -15,6 +15,12 @@ struct DFAState
 	unordered_map<char, DFAState*> transitionsOut;
 };
 
+struct DFAStateFast
+{
+	int id;
+	int transitionsOut[4];
+};
+
 void buildHardcodedDFA( vector<DFAState*>& outDFA ) //Builds DFA for ˆ(a+b+(c|d)+)+$
 {
 	outDFA.clear();
@@ -54,6 +60,50 @@ void buildHardcodedDFA( vector<DFAState*>& outDFA ) //Builds DFA for ˆ(a+b+(c|d)
 	state3->transitionsOut.insert( { 'b', nullptr } );
 	state3->transitionsOut.insert( { 'c', state3 } );
 	state3->transitionsOut.insert( { 'd', state3 } );
+}
+
+void buildHardcodedDFA( DFAStateFast** outDFA, int& size ) //Builds DFA for ˆ(a+b+(c|d)+)+$
+{
+	//outDFA.clear();
+
+	DFAStateFast* state0 = new DFAStateFast();
+	state0->id = 0;
+	//outDFA.push_back( state0 );
+	outDFA[0] = ( state0 );
+
+	DFAStateFast* state1 = new DFAStateFast();
+	state1->id = 1;
+	outDFA[1] = ( state1 );
+
+	DFAStateFast* state2 = new DFAStateFast();
+	state2->id = 2;
+	outDFA[2] = ( state2 );
+
+	DFAStateFast* state3 = new DFAStateFast();
+	state3->id = 3;
+	outDFA[3] = ( state3 );
+
+	state0->transitionsOut[0] = 1;
+	state0->transitionsOut[1] = -1;
+	state0->transitionsOut[2] = -1;
+	state0->transitionsOut[3] = -1;
+
+	state1->transitionsOut[0] = 1;
+	state1->transitionsOut[1] = 2;
+	state1->transitionsOut[2] = -1;
+	state1->transitionsOut[3] = -1;
+
+	state2->transitionsOut[0] = -1;
+	state2->transitionsOut[1] = 2;
+	state2->transitionsOut[2] = 3;
+	state2->transitionsOut[3] = 3;
+
+	state3->transitionsOut[0] = 1;
+	state3->transitionsOut[1] = -1;
+	state3->transitionsOut[2] = 3;
+	state3->transitionsOut[3] = 3;
+
+	size = 4;
 }
 
 int n;
@@ -104,11 +154,12 @@ int main( int argc, char* argv[] )
 	}
 
 	n = atoi( argv[1] );
-	char* strcToMatch = buildString( 1000000 );
+	char* strcToMatch = buildString( 10000000 );
 	string strToMatch( strcToMatch );
 
-	vector<DFAState*> DFA;
-	buildHardcodedDFA( DFA );
+	DFAStateFast** DFA = (DFAStateFast**)malloc( sizeof(DFAStateFast*) * 4 );
+	int DFASize;
+	buildHardcodedDFA( DFA, DFASize );
 
 	int partLen		= strToMatch.length() / ( n + 1 );
 	int partLenRem	= strToMatch.length() % ( n + 1 );
@@ -129,44 +180,41 @@ int main( int argc, char* argv[] )
 	parts.push_back( strToMatch.substr( partPos, strToMatch.length() - n*partLen ) );
 
 	bool mainIsMatch = true;
-	DFAState* mainState = DFA[0];
+	DFAStateFast* mainState = DFA[0];
 
-	omp_set_dynamic( 0 );     // Allows us to enforce number of threads
+	//omp_set_dynamic( 0 );     // Allows us to enforce number of threads
 	omp_set_num_threads( n+1 ); // Use n+1 threads
 
-	vector<vector<DFAState*>> threadStates;
-	threadStates.resize( n );
+	//vector<vector<DFAStateFast*>> threadStates;
+	DFAStateFast*** threadStates = (DFAStateFast***)malloc( sizeof( DFAStateFast** ) * n );
 	for ( int i = 0; i < n; i++ )
 	{
-		threadStates[i].resize( DFA.size() - 1 ); // -1 because we dont consider the entry state for optimistic threads
-		for ( int j = 1; j < DFA.size(); j++ )
+		threadStates[i] = (DFAStateFast**)malloc( sizeof( DFAState* ) * (DFASize - 1)  );
+		for ( int j = 1; j < 4; j++ )
 		{
 			threadStates[i][j - 1] = DFA[j];
 		}
 	}
 	time_t start = clock();
 
-#pragma omp parallel for 
-	for ( int i = 0; i <= n; i++ )
+#pragma omp parallel 
+	//for ( int i = 0; i <= n; i++ )
 	{
+		int i = omp_get_thread_num();
 		if ( i == 0 ) //Normal Thread
 		{
 			string part = parts[i];
 			for ( int strIdx = 0; strIdx < part.length(); strIdx++ )
 			{
-				try
-				{
-					mainState = mainState->transitionsOut.at( part.at( strIdx ) );
-				}
-				catch ( exception e )
-				{
-					mainState = nullptr;
-				}
+				//mainState = mainState->transitionsOut.at( part.at( strIdx ) );
 				if ( mainState == nullptr )
-				{
-					mainIsMatch = false;
 					break;
-				}
+
+				int idx = mainState->transitionsOut[(int)( ( part.at( strIdx ) ) - 'a' )];
+				if ( idx > -1 )
+					mainState = DFA[idx];
+				else
+					mainState = nullptr;
 			}
 		}
 		else //Optimistic Threads
@@ -174,16 +222,16 @@ int main( int argc, char* argv[] )
 			string part = parts[i];
 			for ( int strIdx = 0; strIdx < part.length(); strIdx++ )
 			{
-				for ( int stateIdx = 0; stateIdx < threadStates[i-1].size(); stateIdx++ )
+				for ( int stateIdx = 0; stateIdx < DFASize-1; stateIdx++ )
 				{
-					try
+					if ( threadStates[i - 1][stateIdx] != nullptr )
 					{
-						if( threadStates[i-1][stateIdx] != nullptr )
-							threadStates[i-1][stateIdx] = threadStates[i-1][stateIdx]->transitionsOut.at( part.at( strIdx ) );
-					}
-					catch ( exception e )
-					{
-						threadStates[i-1][stateIdx] = nullptr;
+						int idx = threadStates[i - 1][stateIdx]->transitionsOut[(int)( ( part.at( strIdx ) ) - 'a' )];
+						if( idx > -1 )
+							threadStates[i - 1][stateIdx] = DFA[ idx ];
+						else 
+							threadStates[i - 1][stateIdx] = nullptr;
+						//threadStates[i-1][stateIdx] = threadStates[i-1][stateIdx]->transitionsOut.at( part.at( strIdx ) );
 					}
 				}
 			}
@@ -198,11 +246,18 @@ int main( int argc, char* argv[] )
 			exit( 1 );
 		}
 
-		mainState = mainState->transitionsOut.at( parts[i+1].at( 0 ) );
+		//mainState =  mainState->transitionsOut.at( parts[i+1].at( 0 ) );
+		int idx = mainState->transitionsOut[(int)( ( parts[i + 1].at( 0 ) ) - 'a' )];
+		if( idx > -1 )
+			mainState = DFA[mainState->transitionsOut[(int)( ( parts[i + 1].at( 0 ) ) - 'a' )]];
+		else
+		{
+			break;
+		}
 		if ( mainState->id == 0 )
 		{
-			cout << "No match found" << endl;
-			exit( 1 );
+			mainState = nullptr;
+			break;
 		}
 
 		mainState = threadStates[i][mainState->id - 1];
@@ -219,6 +274,16 @@ int main( int argc, char* argv[] )
 	{
 		cout << "The generated string doesnt match with the given regex" << endl;
 	}
+
+	for ( int i = 0; i < n; i++ )
+	{
+		free(threadStates[i]);
+	}
+	free( threadStates );
+
+	for ( int i = 0; i < DFASize; i++ )
+		delete DFA[i];
+	free( DFA );
 
 	system( "pause" ); //So we can see console output
 }
